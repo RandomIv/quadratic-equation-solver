f0dc5263c170134972d1feb44fcd126ca7b79300
RandomIv <ivanishin.alexander@lll.kpi.ua>
2025-03-12 12:31:23 +0200

    Add readNumber function that can read numbers Add readNumber function that is promisified version of rl.question by using promisify from node:util library and check if it works correctly

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import * as readline from 'readline';
+import { promisify } from 'node:util';
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question('Check reader: ', (answer: string) => {
-  console.log(answer);
-  rl.close();
-});
+const readNumber = promisify(rl.question).bind(rl);
+
+const a = await readNumber('a = ');
+const b = await readNumber('b = ');
+const c = await readNumber('c = ');
+
+rl.close();
+
+console.log([a, b, c]);
