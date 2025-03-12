df8c71d968cc45ccf16987274c2c4bf0b1175419
RandomIv <ivanishin.alexander@lll.kpi.ua>
2025-03-12 17:26:23 +0200

    Separate interactive mode from main

---
+++ src/interactive.ts
@@ -0,0 +1,40 @@
+import * as readline from 'readline';
+import { promisify } from 'node:util';
+import { solveQuadratic } from './quadraticEquationSolver';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const readNumber = promisify(rl.question).bind(rl);
+
+async function getFloat(prompt: string): Promise<string> {
+  while (true) {
+    const input = String(await readNumber(prompt));
+    const number = parseFloat(input);
+
+    if (!isNaN(number)) {
+      return number.toString().includes('.')
+        ? number.toString()
+        : `${number}.0`;
+    }
+
+    console.log(`Error. Expected a valid real number, got "${input}" instead`);
+  }
+}
+export const startInteractiveMode = async () => {
+  const a = String(await getFloat('a = '));
+  const b = String(await getFloat('b = '));
+  const c = String(await getFloat('c = '));
+
+  const result = solveQuadratic(Number(a), Number(b), Number(c));
+
+  rl.close();
+
+  console.log(`
+  Equation is: (${a}) x^2 + (${b}) x + (${c}) = 0
+  There are ${result.length} roots
+  ${result.map((value, index) => `x${index + 1} = ${value}`).join('\n')}
+  `);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,5 @@
-import * as readline from 'readline';
-import { promisify } from 'node:util';
-import { solveQuadratic } from './quadraticEquationSolver';
+import { startInteractiveMode } from './interactive';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const filePath: string = process.argv[2];
 
-const readNumber = promisify(rl.question).bind(rl);
-
-async function getFloat(prompt: string): Promise<string> {
-  while (true) {
-    const input = String(await readNumber(prompt));
-    const number = parseFloat(input);
-
-    if (!isNaN(number)) {
-      return number.toString().includes('.')
-        ? number.toString()
-        : `${number}.0`;
-    }
-
-    console.log(`Error. Expected a valid real number, got "${input}" instead`);
-  }
-}
-const a = String(await getFloat('a = '));
-const b = String(await getFloat('b = '));
-const c = String(await getFloat('c = '));
-
-const result = solveQuadratic(Number(a), Number(b), Number(c));
-
-rl.close();
-
-console.log(`
-Equation is: (${a}) x^2 + (${b}) x + (${c}) = 0
-There are ${result.length} roots
-${result.map((value, index) => `x${index + 1} = ${value}`).join('\n')}
-`);
+if (!filePath) await startInteractiveMode();
